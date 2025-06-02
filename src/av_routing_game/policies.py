47b2d0246c3cf34972cc0cdd0ee6a815f5a44838
Robert Campbell <robertgcampbell95@gmail.com>
2025-06-02 16:50:47 -0700

    Add sample policies

---
+++ src/av_routing_game/policies.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+class Policy:
+    def act(current_location: int, congestion: np.ndarray) -> int:
+        pass
+
+class BeelinePolicy(Policy):
+    def act(current_location: int, congestion: np.ndarray) -> int:
+        pass
+
+class A_star(Policy):
+    def act(current_location: int, congestion: np.ndarray) -> int:
+        pass
+
+class Manual(Policy):
+    def act(current_location: int, congestion: np.ndarray) -> int:
+        pass
